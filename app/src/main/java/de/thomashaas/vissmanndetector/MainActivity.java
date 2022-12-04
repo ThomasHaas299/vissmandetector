67ca4a39a5c63be5c7a0e47a631c4fbf8f155605
th <git@thomashaas.de>
2022-12-04 16:03:18 +0100

    Umstellung nach Int-Values (Faktor 100), damit keine Fließkommaberechnungen nötig sind

--- app/src/main/java/de/thomashaas/vissmanndetector/MainActivity.java
+++ app/src/main/java/de/thomashaas/vissmanndetector/MainActivity.java
@@ -1,7 +1,5 @@
 package de.thomashaas.vissmanndetector;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -10,32 +8,54 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int EVENT_CNT = 100;
     static final int EVENT_PART = 10;
-
+    private final ArrayList<Integer> lst = new ArrayList<>();
     private SensorManager sensorManager;
     private Sensor sensor;
-    private ArrayList<Float> lst = new ArrayList<>();
-
     private TextView tvCurrentValue, tvCalcValue;
     private EditText edOffset;
-    private Button btnSaveOffset, btnStartTransmission, btnStopTransmission;
+    private Button btnStartTransmission;
+    private Button btnStopTransmission;
     private boolean transmitting;
-    private float offset = 0f;
+    private int offset = 0;
+    // Create a listener
+    private final SensorEventListener accelerationSensorListener = new SensorEventListener() {
+        @SuppressWarnings("CommentedOutCode")
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+
+            // String line = String.format(Locale.CANADA, "%.2f %.2f %.2f", sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]);
+            // log(line);
+
+            int value = (int) (Math.abs(sensorEvent.values[2]) * 100);
+
+            lst.add(value);
+
+            if (lst.size() >= EVENT_CNT) {
+                mCalc();
+            }
+
+            // Log.d("VSMN", String.valueOf(sensorEvent.values[2]));
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+        }
+    };
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -50,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         tvCurrentValue = findViewById(R.id.tvCurrentValue);
         tvCalcValue = findViewById(R.id.tvCalcValue);
         edOffset = findViewById(R.id.edOffset);
-        btnSaveOffset = findViewById(R.id.btnSaveOffset);
+        Button btnSaveOffset = findViewById(R.id.btnSaveOffset);
         btnStartTransmission = findViewById(R.id.btnStartTransmission);
         btnStopTransmission = findViewById(R.id.btnStopTransmission);
 
@@ -83,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         log("save Offset");
 
         try {
-            offset = Float.parseFloat(edOffset.getText().toString());
+            offset = Integer.parseInt(edOffset.getText().toString());
             Toast.makeText(this, "Offset gespeichert", Toast.LENGTH_SHORT).show();
         } catch (NumberFormatException e) {
             e.printStackTrace();
@@ -91,50 +111,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // Create a listener
-    private final SensorEventListener accelerationSensorListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent sensorEvent) {
-
-            // String line = String.format(Locale.CANADA, "%.2f %.2f %.2f", sensorEvent.values[0], sensorEvent.values[1], sensorEvent.values[2]);
-            // log(line);
-
-            lst.add(Math.abs(sensorEvent.values[2]));
-
-            if (lst.size() >= EVENT_CNT) {
-                mCalc();
-            }
-
-            // Log.d("VSMN", String.valueOf(sensorEvent.values[2]));
-
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int i) {
-        }
-    };
-
     private void mCalc() {
-        log("mCalc");
-        ArrayList<Float> nLst = new ArrayList<>(lst);
+        // log("mCalc");
+        ArrayList<Integer> nLst = new ArrayList<>(lst);
 
         lst.clear();
         nLst.sort(Collections.reverseOrder());
-        float sum = 0f;
+
+        // log(nLst.toString());
+
+        int sum = 0;
         for (int i = 0; i < EVENT_PART; i++) {
             sum += nLst.get(i);
         }
-        float ave = sum / (float) EVENT_PART;
+        int ave = sum / EVENT_PART;
 
-        log(String.valueOf(ave));
+        // log(String.valueOf(ave));
 
         tvCurrentValue.setText(String.valueOf(ave));
 
-        float calcValue = Math.abs(ave - offset);
+        int calcValue = Math.abs(ave - offset);
         tvCalcValue.setText(String.valueOf(calcValue));
 
     }
 
+    @SuppressWarnings("SameParameterValue")
     private void log(String s) {
         Log.d("VSMN", s);
     }
